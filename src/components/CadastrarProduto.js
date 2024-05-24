c45ae4ddf062fb04e8a4a4cc672f70f6172708a5
Iuri Teixeira <84046265+IuriTeixeira@users.noreply.github.com>
2024-05-24 19:06:27 -0400

    front das 2 primeiras telas da sprint 1

---
+++ src/components/CadastrarProduto.js
@@ -0,0 +1,54 @@
+function UploadImagem () {
+    return (
+        <div>
+            <form className="image-upload">
+                <label for="input-imagem">
+                    <img src="img/UploadImagem.png" alt="Escolha sua imagem"/>
+                </label>
+                <input type="file" accept="image/*" id="input-imagem" />
+            </form>
+        </div>
+    );
+}
+
+function Formularios () {
+    return (
+        <div>
+            <form id="FormProduto" method="POST">
+                <fieldset>
+                    <div>
+                        <label for="NomeProduto" className="form-label mt-4">Nome</label>
+                        <input type="text" className="form-control rounded-pill" id="NomeProduto" placeholder="Adicione o nome do produto" />
+                    </div>
+                    <div>
+                        <label for="MarcaProduto" className="form-label mt-4">Marca</label>
+                        <input type="text" className="form-control rounded-pill" id="MarcaProduto" placeholder="Adicione a marca do produto" />
+                    </div>
+                    <div>
+                        <label for="Descricao" className="form-label mt-4"><strong>Descrição</strong></label>
+                        <textarea className="form-control rounded-pill" id="exampleTextarea" name="Descricao" placeholder="Adicione uma descrição detalhada do produto" rows="3"></textarea>
+                    </div>
+                    <div>
+                        <label for="IngredientesProduto" className="form-label mt-4">Ingredientes</label>
+                        <input type="text" className="form-control rounded-pill" id="IngredientesProduto" placeholder="Adicione os ingredientes do produto, para cada ingrediente use uma # antes" />
+                    </div>
+                    <br />
+                    <input type="submit" className="btn btn-dark rounded-pill" value="Publicar" />
+                </fieldset>
+            </form>
+        </div>
+    );
+}
+
+export default function CadastrarProduto () {
+    return (
+        <div className="splitScreen">
+            <div>
+                <UploadImagem />
+            </div>
+            <div className="form-produto">
+                <Formularios />
+            </div>
+        </div>
+        )
+}
